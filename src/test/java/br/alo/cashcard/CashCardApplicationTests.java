b9a963da5b575deafd46a034de3b01dfde58b9c3
Andre Luis Olivete <olivete@ifsp.edu.br>
2023-11-20 20:42:13 -0300

    Criando o projeto Spring com Gradle.

---
+++ src/test/java/br/alo/cashcard/CashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package br.alo.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
